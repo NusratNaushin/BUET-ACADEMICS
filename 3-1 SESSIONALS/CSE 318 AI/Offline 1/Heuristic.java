037f9b214c2721df2bbc91d549c011a07b608bfb
Nusrat Jahan Nidhi <nusrat.nidhi7801@gmail.com>
2025-04-17 13:10:32 +0600

    added offline 1

---
+++ 3-1 SESSIONALS/CSE 318 AI/Offline 1/Heuristic.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class Heuristic {
+    public static int calculateHeuristic(int[][] puzzle , String hname ){
+        String heuristicName = hname.toLowerCase();
+        if(heuristicName.equals("hamming")){
+            return hamming(puzzle);
+        }
+
+        else if(heuristicName.equals("manhattan")){
+            return manhattan(puzzle);
+        }
+
+        else if(heuristicName.equals("euclidian")){
+            return euclidian(puzzle);
+        }
+
+        else if(heuristicName.equals("linear")){
+            return linear(puzzle);
+        }
+
+       
+    }
+
+    public static int hamming(int[][] puzzle){
+        int count = 0 ;
+        int value = 1 ;
+        int size = puzzle.length;
+
+        for(int i = 0 ; i < size ; i ++){
+            for(int j = 0 ; j < size ; j++, value++){
+                if(puzzle[i][j] !=0 && puzzle[i][j] != value){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int manhattan(int[][] puzzle){
+        int distance = 0 ;
+        int size = puzzle.length;
+        for(int i = 0 ; i < size ; i++){
+            for(int j = 0; j < size ; j++){
+                int value = puzzle[i][j];
+
+                if(value != 0){
+                    int targetX = (value - 1)/size;
+                    int targetY = (value - 1)%size;
+                    distance = distance + Math.abs(targetX - i) + Math.abs(targetY - j);
+                }
+            }
+        }
+
+
+        return distance;
+    }
+
+
+    public static int euclidian(int[][] puzzle){
+        int distance = 0 ;
+        int size = puzzle.length;
+        for(int i = 0 ; i < size ; i++){
+            for(int j = 0 ; j < size ; j++){
+                int value = puzzle[i][j];
+
+                if(value != 0){
+                    int targetX = (value - 1)/size;
+                    int targetY = (value - 1)%size;
+                    int squareOfX = (int)Math.pow(i - targetX ,2);
+                    int squareOfY = (int)Math.pow(j - targetY ,2);
+                    distance = distance + (int)Math.sqrt(squareOfX + squareOfY);
+
+                }
+            }
+        }
+
+        return (int)distance;
+    }
+
+    public static int linearConflict(int[][] puzzle){
+        
+
+    }
+
+
+}
