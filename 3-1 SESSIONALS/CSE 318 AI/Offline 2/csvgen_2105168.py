55224bda9296e2e300a7de5f7c0ecea4bdadc247
Nusrat Jahan Nidhi <nusrat.nidhi7801@gmail.com>
2025-05-10 12:34:35 +0600

    Done

---
+++ 3-1 SESSIONALS/CSE 318 AI/Offline 2/csvgen_2105168.py
@@ -0,0 +1,135 @@
+import csv
+import os 
+import re
+from main_2105168 import Graph, Algorithm
+
+
+iterations = 10
+alpha = 0.9
+k = 5
+
+def sort_files(s):
+    parts = re.split(r'(\d+)', s)
+    processed = []
+
+    for part in parts:
+        if part.isdigit():
+            processed.append(int(part))
+        else:
+            processed.append(part.lower())
+    
+    return processed
+
+
+
+def read_a_input_file(filepath):
+    with open(filepath , 'r') as f:
+        n , m = map(int , f.readline().split())
+        G = Graph(n,m)
+        for _ in range(m):
+            u,v,w = map(int , f.readline().split())
+            G.add_edge(u,v,w)
+    return G
+
+def run_all_input_files(input_directory , output_csv , knownbestdictionary):
+    results = []
+
+
+    for filename in sorted(os.listdir(input_directory), key=sort_files):
+        if filename.endswith(".rud"):
+            filepath = os.path.join(input_directory, filename)
+            G = read_a_input_file(filepath)
+
+            name = filename[:-4]
+            n = len(G.nodes)
+            m = len(G.edges) // 2
+
+            rand_cut , Xrand , Yrand = Algorithm.RandomizedMaxCut(G , iterations)
+            _,_, greedy_cut = Algorithm.Greedy(G)
+            XsemiValue , YsemiValue , semi_value = Algorithm.SemiGreedyValueBased(G , alpha )
+            Xcard , Ycard , semi_card = Algorithm.SemiGreedy_CardinalityBasedRCL(G , k)
+            _, _, local_cut_value, local_iters_value = Algorithm.LocalSearch(G, XsemiValue, YsemiValue)
+            _, _, local_cut_card, local_iters_card = Algorithm.LocalSearch(G, Xcard, Ycard)
+
+            _, _, grasp_cut_1, grasp_iters_1 = Algorithm.GRASP(G, iterations, 1, 0.9, 5)
+            _, _, grasp_cut_2, grasp_iters_2 = Algorithm.GRASP(G, iterations, 2, 0.9, 5)           
+
+
+            results.append([
+
+                name, n, m,
+                int(rand_cut), greedy_cut, semi_value,semi_card,
+                local_iters_value, local_cut_value,
+                local_iters_card, local_cut_card,
+                grasp_iters_1, grasp_cut_1,
+                grasp_iters_2, grasp_cut_2,
+                knownbestdictionary.get(name.upper(), "")
+
+            ])
+    
+    with open(output_csv , 'a' , newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([
+            "Problem", "", "",
+            "Constructive Algorithm", "", "","",
+            "Local Search", "", "", "",
+            "GRASP", "", "", "",
+            "Known best Solution or upper bound"
+        ])
+
+        writer.writerow([
+            "Name", "|V| or n", "|E| or m",
+            "Simple Randomized", "Greedy-1", "Semi-Greedy-1", "Semi-Greedy-2",
+            "Local Search for Semi-Greedy-1", "", 
+            "Local Search for Semi-Greedy-2", "", 
+            "GRASP for Semi-Greedy-1", "", 
+            "GRASP for Semi-Greedy-2", "", 
+            "Known Best"
+        ])
+
+        writer.writerow([
+            "", "", "",
+            "", "", "", "", 
+            "Number of Iterations", "Best Value",
+            "Number of Iterations", "Best Value",
+            "Number of Iterations", "Best Value",
+            "Number of Iterations", "Best Value",
+            ""
+        ])
+
+
+        writer.writerows(results)
+
+known_best = {
+    "G1": 12078,
+    "G2": 12084,
+    "G3": 12077,
+    "G11": 627,
+    "G12": 621,
+    "G13": 645,
+    "G14": 3187,
+    "G15": 3169,
+    "G16": 3172,
+    "G22": 14123,
+    "G23": 14129,
+    "G24": 14131,
+    "G32": 1560,
+    "G33": 1537,
+    "G34": 1541,
+    "G35": 8000,
+    "G36": 7996,
+    "G37": 8009,
+    "G43": 7027,
+    "G44": 7022,
+    "G45": 7020,
+    "G48": 6000,
+    "G49": 6000,
+    "G50": 5988
+}
+
+run_all_input_files("graph_GRASP/set1", "2105168_final.csv" , known_best)
+
+#957 - 1037
+#1049 -
+
+
---
+++ 3-1 SESSIONALS/CSE 318 AI/Offline 2/plotMaxCut_2105168.py
@@ -0,0 +1,75 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+
+graph1 = 1
+graph2 = 10
+
+def plot_from_csv(csv_file):
+    with open(csv_file,'r') as f:
+        reader = list(csv.reader(f))
+    
+    data = reader[2:]
+
+    graph_names = []
+
+    random_cut = []
+    greedy_cut =[]
+    semi_greedy_cut_value = []
+    #semi_greedy_cut_card = []
+    local_search_cut =[]
+    grasp_cut =[]
+    
+    for row in data:
+        try:
+            name = row[0]
+            match = re.match(r'g(\d+)', name)
+            if not match:
+                continue
+            number = int(match.group(1))
+            if number < graph1 or graph2 < number:
+                continue
+            
+
+            rand = int(row[3])
+            greed = int(row[4])
+            semi = int(row[5])
+            grasp = int(row[12])
+            local = int(row[8])
+        except (ValueError , IndexError):
+            continue
+
+
+        graph_names.append(name)
+        random_cut.append(rand)
+        greedy_cut.append(greed)
+        semi_greedy_cut_value.append(semi)
+        grasp_cut.append(grasp)
+        local_search_cut.append(local)
+
+    index = np.arange(len(graph_names))
+    width = 0.15
+
+    plt.figure(figsize=(14,7))
+
+
+    plt.bar(index - 2*width , random_cut , width=width ,label = 'Randomized' , color= 'skyblue')
+    plt.bar(index - width , greedy_cut , width=width ,label='Greedy' , color= 'orange')
+    plt.bar(index  , semi_greedy_cut_value , width=width ,label='Semi-Greedy', color= 'gray')
+    plt.bar(index + width , grasp_cut , width=width ,label='GRASP' , color= 'gold')
+    plt.bar(index + 2*width , local_search_cut , width=width ,label='Local Search' , color= 'blue')
+
+
+    
+    plt.xticks(index, graph_names, rotation=45)
+    plt.xlabel("Graphs")
+    plt.ylabel("Cut Value")
+    plt.title("Max Cut for Graphs g1 to g10")
+    plt.legend()
+    plt.grid(True, axis='y', linestyle='--', linewidth=0.5)
+    plt.tight_layout()
+    plt.savefig("max_cut_g1_to_g10.png")
+    plt.show()
+
+plot_from_csv("2105168_final.csv")
